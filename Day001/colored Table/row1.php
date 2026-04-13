6140457eeb620158cdfa166bfc304fbba142e205
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-13 18:26:21 +0200

    PHP files for table rows with dynamic background colors

---
+++ Day001/colored Table/row1.php
@@ -0,0 +1,5 @@
+<?php
+ 
+$row ="	<tr style = 'background-color : $color1 ;'><td></td><td></td><td></td><td></td> </tr> "
+		
+?>
---
+++ Day001/colored Table/row4.php
@@ -0,0 +1,4 @@
+<?php
+$row ="	<tr style = 'background-color :  $color4 ;'><td></td><td></td><td></td><td></td> </tr> "
+
+?>
