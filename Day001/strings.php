eb9cc4843eb231677713c2633faeb3b28e46857a
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-13 17:04:02 +0200

    Konkatenation, Escaping, Interpolation

--- Day001/strings.php
+++ Day001/strings.php
@@ -1,9 +1,9 @@
 <?php
 
 	$text1 = "Guten Morgen Berlin";
-	$text2 = 'Guten Morgen Berlin';
-	$text3 = 'Guten Morgen "Berlin"';
-	$text4 = "Guten Morgen \"Berlin\"";
+	$text2 = 'Guten Morgen Berlin'; /*Kein Unterschied zwischen Eifach/doppelte - Anführungszeichen*/
+	$text3 = 'Guten Morgen "Berlin"'; /* außen '...' , innen "..." → kein Problem*/
+	$text4 = "Guten Morgen \"Berlin\"";/*außen "...", innen auch "..." , deshalb escapen: \"*/
 
 	echo $text1 . "<br>";
 	echo $text2 . "<br>";
@@ -11,12 +11,16 @@
 	echo $text4 . "<br>";
 
 	$ort = "Berlin";
-	$text5 = 'Guten Morgen "$ort"';
+	$text5 = 'Guten Morgen "$ort"'; /*❌ Kein Interpolation mit einfachen Quotes*/
 	echo $text5 . "<br>";
 	
-	$text6 = "Guten Morgen \"$ort\"";
+	$text6 = "Guten Morgen \"$ort\"";# ✅ Interpolation mit doppelten Quotes
 	echo $text6 . "<br>";
+	
+	/*
+		"..." → Interpolation möglich
+		'...' → keine Interpolation, reiner Text
+	*/
 
-
-	echo "Hallo $ort mit Interpolation ersetzt";
+	echo "Hallo $ort mit Interpolation ersetzt"; # Interpolieren im echo
 	?>
