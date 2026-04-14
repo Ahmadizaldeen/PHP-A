16ee495c48c849dbdb955fa6505150e3dbced0e2
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-14 19:12:56 +0200

    PHP start Scripts

---
+++ Day002/website1/navigation.php
@@ -0,0 +1,7 @@
+<hr>
+<a href = "index.php">home</a>
+<a href = "seite1.php">Seite1</a>
+<a href = "seite2.php">Seite2</a>
+<a href = "seite3.php">Seite3</a>
+<a href = "seite4.php">Seite4</a>
+<hr>
