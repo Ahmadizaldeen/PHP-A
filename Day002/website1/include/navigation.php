8d52a6ea4da5da728d2598ce45d43e4e0c39888f
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-15 20:54:57 +0200

    Navbar script

---
+++ Day002/website1/include/navigation.php
@@ -0,0 +1,7 @@
+<hr>
+<a href = "index.php">home</a>
+<a href = "seite1.php">CreateTable</a>
+<a href = "seite2.php">KontaktForm</a>
+<a href = "seite3.php">Lotto</a>
+<a href = "seite4.php">Colors</a>
+<hr>
--- Day002/website1/navigation.php
+++
@@ -1,7 +0,0 @@
-<hr>
-<a href = "index.php">home</a>
-<a href = "seite1.php">Seite1</a>
-<a href = "seite2.php">Seite2</a>
-<a href = "seite3.php">Seite3</a>
-<a href = "seite4.php">Seite4</a>
-<hr>
