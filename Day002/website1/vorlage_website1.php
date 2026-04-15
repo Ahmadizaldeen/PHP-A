fc5ac9a475aa0fba3d4fdbc71dcd4984440a1181
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-15 20:55:35 +0200

    minimale Vorlage alle Seiten

---
+++ Day002/website1/vorlage_website1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<title>Vorlage</title>
+	<style></style>
+	<link rel = "stylesheet" href = "css/style.css">
+	<script src = "js/script.js"></script>
+</head>
+<body>
+	<header><h1>Vorlage</h1></header>
+	
+	<nav>
+		<?php require_once "include/navigation.php"; ?> 
+	</nav>
+	
+	<main>
+		<?php 	?>
+	</main>
+	<footer>
+		<p>&copy; Me, Myself & I 2026</p>
+		<?php require_once "include/navi_footer.php"; ?>
+	</footer>
+</body>
+</html>
