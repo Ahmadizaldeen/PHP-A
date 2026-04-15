dc584b03581671db142eafa8cec647a35dc0b5dd
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-15 21:03:15 +0200

    Add Navbars, einbinden mit lotto_generator

--- Day002/website1/seite3.php
+++ Day002/website1/seite3.php
@@ -1,18 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>My Website</title>
+	<meta charset = "utf-8">
+	<title>Lotto spielen</title>
+	<style></style>
 	<link rel = "stylesheet" href = "css/style.css">
 	<script src = "js/script.js"></script>
 </head>
 <body>
-	<header>My Website</header>
+	<header><h1>Vorlage</h1></header>
+	
 	<nav>
-		<?php
-			require_once "navigation.php";
-		?>
+		<?php require_once "include/navigation.php"; ?> 
 	</nav>
-	<main></main>
-	<h1>Seite3</h1>
-	<footer></footer>
+	
+	<main>
+		<?php require_once "include/lotto_generator.php";	?>
+	</main>
+	<footer>
+		<p>&copy; Me, Myself & I 2026</p>
+		<?php require_once "include/navi_footer.php"; ?>
+	</footer>
 </body>
 </html>
